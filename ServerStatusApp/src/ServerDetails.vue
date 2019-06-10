f0ba07cc08b95f6583215abdda355c4c27277bc8
Ivaylo Stefanov <ivaylo.stefanov@strypes.eu>
2019-06-10 18:17:33 +0300

    Server status app using local components.

---
+++ ServerStatusApp/src/ServerDetails.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="row">
+      <div class="col-xs-12 col-sm-6">
+          <p>Server Details are currently not updated</p>
+      </div>
+   </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
