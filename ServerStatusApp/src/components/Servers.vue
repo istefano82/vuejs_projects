35259a5573e149c2d142c5866ea11c7d45cd2a93
Ivaylo Stefanov <ivaylo.stefanov@strypes.eu>
2019-06-14 14:40:09 +0300

    Server app now uses individual server components.
    Server and ServerDetail components communiate via main event bus.

--- ServerStatusApp/src/components/Servers.vue
+++ ServerStatusApp/src/components/Servers.vue
@@ -2,23 +2,33 @@
    <div class="row">
       <div class="col-xs-12 col-sm-6">
           <ul class="list-group">
-              <li
-                  class="list-group-item"
-                  v-for="index in 5">
-                  Server #{{ index }}
-              </li>
-
+            <app-server
+            v-for="server in servers"
+            :server="server"> </app-server>
           </ul>
       </div>
    </div>
 </template>
 
 <script>
-
+    import Server from './Server.vue'
+    export default {
+      components: {
+        'app-server': Server
+      },
+        data: function(){
+            return {
+                servers: [
+                  {id:1, status: 'Normal'},
+                  {id:2, status: 'Critical'},
+                  {id:3, status: 'Unknown'},
+                  {id:4, status: 'Normal'}
+                ]
+            };
+        },
+    }
 </script>
 
 <style scoped>
-div {
-  border: 1px solid blue;
-}
+
 </style>
